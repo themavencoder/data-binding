41685e273d156256e820f09725f4c3bd9db66f0c
Tobiloba Adejumo <eniolaplus08@gmail.com>
2019-03-02 14:46:17 +0100

    Ready data binding

--- app/src/main/java/com/aloineinc/movie_app/authentication/LoginActivity.java
+++ app/src/main/java/com/aloineinc/movie_app/authentication/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.aloineinc.movie_app.authentication;
 
 import android.content.SharedPreferences;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,13 +12,29 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 
 import com.aloineinc.movie_app.R;
+import com.aloineinc.movie_app.authentication.model.LoginHandler;
+import com.aloineinc.movie_app.authentication.model.User;
+import com.aloineinc.movie_app.databinding.LoginActivityBinding;
 
 public class LoginActivity extends AppCompatActivity {
+    //create an object of the generated class
+    private LoginActivityBinding binding;
+    private User user;
+    private LoginHandler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        //inflate the layout using the predefined DataBindingUtil class
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_login);
+        //instantiate the created object
+        user = new User();
+        //parse the object to the handler constructor
+        handler = new LoginHandler(user);
+        //set the user model to the binding class
+        binding.setUser(user);
+        //set the handler object to the binding class
+        binding.setHandler(handler);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
